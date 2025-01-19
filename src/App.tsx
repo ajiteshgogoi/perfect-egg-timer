d20b1ef81aa76f71fb0d13a1ad0f39bb6d0ad35d
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-01-19 09:26:57 +0530

    Change footer formating

--- src/App.tsx
+++ src/App.tsx
@@ -472,12 +472,12 @@ const App: React.FC = () => {
         </div>
       )}
         </div>
-        <div className="mt-auto mt-2 mb-2 py-4 flex flex-col items-center space-y-1 z-30 w-full">
+<div className="mt-8 mb-2 py-4 flex flex-col items-center space-y-2 z-30 w-full">
           <a 
             href="https://ko-fi.com/gogoi" 
             target="_blank" 
             rel="noopener noreferrer"
-            className="bg-[#E84A8F] text-white font-bold py-1.5 px-3 rounded-2xl transition-all duration-300 active:scale-95 flex items-center space-x-2 text-xs hover:bg-[#F05FA4] hover:shadow-[0_4px_12px_rgba(232,74,143,0.2)]"
+            className="bg-[#E84A8F] text-white font-bold py-2 px-4 rounded-2xl transition-all duration-300 active:scale-95 flex items-center space-x-2 text-sm hover:bg-[#F05FA4] hover:shadow-[0_4px_12px_rgba(232,74,143,0.2)]"
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
               <path d="M23.881 8.948c-.773-4.085-4.859-4.593-4.859-4.593H.723c-.604 0-.679.798-.679.798s-.082 7.324-.022 11.822c.164 2.424 2.586 2.672 2.586 2.672s8.267-.023 11.966-.049c2.438-.426 2.683-2.566 2.658-3.734 4.352.24 7.422-2.831 6.649-6.916zm-11.062 3.511c-1.246 1.453-4.011 3.976-4.011 3.976s-.121.119-.31.023c-.076-.057-.108-.09-.108-.09-.443-.441-3.368-3.049-4.034-3.954-.709-.965-1.041-2.7-.091-3.71.951-1.01 3.005-1.086 4.363.407 0 0 1.565-1.782 3.468-.963 1.904.82 1.832 3.011.723 4.311zm6.173.478c-.928.116-1.682.028-1.682.028V7.284h1.77s1.971.551 1.971 2.638c0 1.913-.985 2.667-2.059 3.015z"/>
